dd76663920171171a018877388412f9cdd57337f
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2022-08-26 05:25:06 +0100

    fix: hero sectio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,13 +31,6 @@ const Hero = ({
           style={{ background: "#B4A9F7", backgroundColor: pictureBgColor }}
           className="relative w-[72px] h-[72px] rounded-full flex items-center justify-center "
         >
-          {/* <Image
-            className="absolute bottom-0 "
-            src="/vectors/default-pic.svg"
-            alt="default-img"
-            width={72}
-            height={72}
-          /> */}
           <span className=" text-[3.5rem] lg:text-[2.8rem] ">👨‍💻</span>
         </div>
       )}
@@ -45,10 +38,10 @@ const Hero = ({
         <span className="text-sm dark:text-white font-manrope">
           Hello,I&apos;m
         </span>
-        <h3 className="text-h-black my-1  text-center dark:text-white text-[1.7rem] md:text-[4rem] leading-none ">
+        <h3 className="text-h-black my-1  text-center dark:text-white text-[2.5rem] md:text-[4rem] leading-none ">
           {name}
         </h3>
-        <span className="text-h-grey text-[1.7rem] text-center md:text-[4rem] leading-none">
+        <span className="text-h-grey text-[2.5rem] text-center md:text-[4rem] leading-none">
           {role}
         </span>
       </div>
