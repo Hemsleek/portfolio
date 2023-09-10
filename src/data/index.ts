51366f4ab91a9a9572da95fea83ebc46ee88248d
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2023-09-10 11:23:19 +0100

    update: experience

--- src/data/index.ts
+++ src/data/index.ts
@@ -13,7 +13,7 @@ export const HeroData = {
 
 export const aboutData = {
   about:
-    "Professional and Versatile Frontend Engineer who specializes in web and mobile software development with 4+ years of experience building user-friendly applications. I am a proactive team player and have a core strong belief in communication, I am passionately and actively seeking new technologies on the fly to stay up-to-date on industry trends and advancements, and also work as a team to build and optimize performance for better achieving comprehensive solutions.  In my spare time, I explore the net to pick up new technologies and enjoy socializing.",
+    "Professional and Versatile Frontend Engineer who specializes in web and mobile software development with 5 years of experience building user-friendly applications. I am a proactive team player and have a core strong belief in communication, I am passionately and actively seeking new technologies on the fly to stay up-to-date on industry trends and advancements, and also work as a team to build and optimize performance for better achieving comprehensive solutions.  In my spare time, I explore the net to pick up new technologies and enjoy socializing.",
 };
 
 export const techTools = {
